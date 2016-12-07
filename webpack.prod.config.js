6fd12cd1ad465f725d51319d2a4fc9d4b2ec0c7a
alejandro garcia <agarcia@app-press.com>
2016-12-07 23:28:28 +0100

    Updated defaults index.html in build and dev

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -28,9 +28,10 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
       }
     })
   ],
